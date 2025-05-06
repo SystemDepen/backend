de092cceba61b3b8e8959d45aa9120d3ea459cc1
Kauã Werle <kauawerle19@gmail.com>
2025-05-06 18:58:26 -0300

    feat: Implement new build app using .war

--- src/main/java/com/github/sysdepen/depen_api/DepenApiApplication.java
+++ src/main/java/com/github/sysdepen/depen_api/DepenApiApplication.java
@@ -2,12 +2,20 @@ package com.github.sysdepen.depen_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DepenApiApplication {
+@ComponentScan(basePackages = "com.github.sysdepen.depen_api")
+public class DepenApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepenApiApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DepenApiApplication.class);
+	}
 }
