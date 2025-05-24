577959abc7260e5e9834c00eca80dc508a531627
Kauã Werle <kauawerle19@gmail.com>
2025-05-23 21:24:59 -0300

    fix keycloak

--- src/main/java/com/github/sysdepen/depen_api/security/config/GlobalCorsConfig.java
+++ src/main/java/com/github/sysdepen/depen_api/security/config/GlobalCorsConfig.java
@@ -1,36 +1,36 @@
-package com.github.sysdepen.depen_api.security.config;
-
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.context.annotation.*;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class GlobalCorsConfig {
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        // esquema + host (sem porta 80, porque é default)
-        config.addAllowedOrigin("http://frontend.local.sysdepen.com.br");
-        // qualquer header — isto garante que Content-Type será liberado
-        config.addAllowedHeader("*");
-        // os métodos que você usa
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("OPTIONS");
-        // se você precisar enviar cookies/token via cookie
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-}
+//package com.github.sysdepen.depen_api.security.config;
+//
+//import org.springframework.web.filter.CorsFilter;
+//import org.springframework.context.annotation.*;
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class GlobalCorsConfig {
+//
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        // esquema + host (sem porta 80, porque é default)
+//        config.addAllowedOrigin("http://frontend.local.sysdepen.com.br");
+//        // qualquer header — isto garante que Content-Type será liberado
+//        config.addAllowedHeader("*");
+//        // os métodos que você usa
+//        config.addAllowedMethod("GET");
+//        config.addAllowedMethod("POST");
+//        config.addAllowedMethod("PUT");
+//        config.addAllowedMethod("DELETE");
+//        config.addAllowedMethod("OPTIONS");
+//        // se você precisar enviar cookies/token via cookie
+//        config.setAllowCredentials(true);
+//        config.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+//}
--- src/main/java/com/github/sysdepen/depen_api/security/config/SecurityConfig.java
+++ src/main/java/com/github/sysdepen/depen_api/security/config/SecurityConfig.java
@@ -1,31 +1,22 @@
 package com.github.sysdepen.depen_api.security.config;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,16 +26,17 @@ public class SecurityConfig  {
 	@Order(1)
 	public SecurityFilterChain publicLoginChain(HttpSecurity http) throws Exception {
 		http
-				.securityMatcher(new OrRequestMatcher(
-						new AntPathRequestMatcher("/api/v1/login/logar"),
-						new AntPathRequestMatcher("/api/v1/usuario/save")   // <- pega /save e /save/
-				))
-				.cors(Customizer.withDefaults())
+				// só vale pra esse endpoint
+				.securityMatcher("/api/v1/login/logar", "/api/v1/usuario/save")
 				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(a -> a.anyRequest().permitAll());
+				.cors(Customizer.withDefaults())
+				.authorizeHttpRequests(auth -> auth
+						.anyRequest().permitAll()
+				);
 		return http.build();
 	}
 
+	// 2) Chain “privada” para toda a API – aqui sim entra o JWT Resource Server
 	@Bean
 	@Order(2)
 	public SecurityFilterChain apiChain(HttpSecurity http) throws Exception {
@@ -52,45 +44,40 @@ public class SecurityConfig  {
 				.securityMatcher("/api/**")
 				.cors(Customizer.withDefaults())
 				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(a -> a.anyRequest().authenticated())
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+				.sessionManagement(sm ->
+						sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				.authorizeHttpRequests(auth -> auth
+						// preflights continuam liberados
+						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.oauth2ResourceServer(oauth2 ->
+						oauth2.jwt(Customizer.withDefaults())
+				)
+				.exceptionHandling(ex ->
+						ex.authenticationEntryPoint(
+								new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+						)
+				);
 		return http.build();
 	}
 
-//	@Bean
-//	@Order(2)
-//	public SecurityFilterChain apiChain(HttpSecurity http) throws Exception {
-//		http
-//				.securityMatcher("/api/**")
-//				.cors(Customizer.withDefaults())
-//				.csrf(csrf -> csrf.disable())
-//				.authorizeHttpRequests(a -> a
-//						// libera todos os pre-flights (OPTIONS)
-//						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//						// o resto exige JWT
-//						.anyRequest().authenticated()
-//				)
-//				.oauth2ResourceServer(o -> o.jwt(Customizer.withDefaults()));
-//		return http.build();
-//	}
-
 	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
+	public FilterRegistrationBean<org.springframework.web.filter.CorsFilter> corsFilterRegistration() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		// liste exatamente o seu front (sem "*")
-		config.setAllowedOriginPatterns(
-				List.of("http://frontend.local.sysdepen.com.br")
-		);
-		config.setAllowedMethods(
-				List.of("GET","POST","PUT","DELETE","OPTIONS")
-		);
+		config.addAllowedOriginPattern("*");
+		config.setAllowedOriginPatterns(List.of("https://frontend.local.sysdepen.com.br", "100.95.243.7:443"));
+		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
 		config.setMaxAge(3600L);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
-		return source;
+		FilterRegistrationBean<org.springframework.web.filter.CorsFilter> bean =
+				new FilterRegistrationBean<>(new org.springframework.web.filter.CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
